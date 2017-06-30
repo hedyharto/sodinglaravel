80f8d333e9ae541d45f3651996073e6ae37ba602
hedyharto <hedy@gildartz.com>
2017-06-30 16:54:43 +0800

    -

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -12,24 +12,30 @@
                     <table class="table">
                         <thead>
                             <tr>
+                                <th>#</th>
                                 <th>User</th>
                                 <th>Name</th>
                                 <th>Description</th>
                                 <th>Created</th>
                                 <th>Updated</th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
 
                             @if($tasks)
 
-                                @foreach($tasks as $task)
+                                @foreach($tasks as $no=>$task)
                                     <tr>
+                                        <th>{{$no+1}}</th>
                                         <td>{{$task->user->name}}</td>
                                         <td>{{$task->name}}</td>
                                         <td>{{$task->description}}</td>
                                         <td>{{$task->created_at}}</td>
                                         <td>{{$task->updated_at}}</td>
+                                        <td>
+                                            <a href="#" type="button" class="btn btn-warning btn-xs">Edit</a>
+                                        </td>
                                     </tr>
                                 @endforeach
 
